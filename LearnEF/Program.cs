2a38956209293941dd66a8c362e8781f4f1d64bd
Jason Guo <jiazheng.guo@outlook.com>
2025-06-15 10:13:47 -0400

    enable OpenAPI routes

--- LearnEF/Program.cs
+++ LearnEF/Program.cs
@@ -21,11 +21,7 @@ builder.Services.AddDbContext<TestDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
